39b03c6bae2aaa75154c6b43708e83f53dfeca7a
ParthMicro <goyalparth058@gmail.com>
2025-03-22 11:09:10 +0530

    Added Get all Contacts method

--- App.js
+++ App.js
@@ -91,6 +91,11 @@ class AddressBook {
             console.error('Contact not found.');
         }
     }
+
+    //count the number of contacts
+    getContactCount() {
+        return this.contacts.reduce(count => count + 1, 0);
+    }
 }
 
 const addressBook = new AddressBook();
@@ -106,3 +111,6 @@ addressBook.findAndEditContact("Parth",{city: "NewCity", phone: "9752008224"});
 addressBook.findAndDeleteContact("Abc");
 addressBook.findAndDeleteContact("Shanya");
 console.log(JSON.stringify(addressBook.contacts,null,2));
+
+// Display contact count
+console.log("Number of contacts in address book:", addressBook.getContactCount());
